99db8c35d1250da1243ce1c4230c03be84217986
Daniel Rieck <danielcrieck@gmail.com>
2017-06-02 14:14:48 -0700

    Incorporating *.less compiling into gulp automation task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
-var gulp = require('gulp'),
+var compiledCssSrcFileName = 'pbk-custom.css',
+	minCssFileHeaderStr = '/* Built with the LESS CSS preprocessor [http://lesscss.org/]. Please see [https://github.com/invokeImmediately/phibetakappa.wsu.edu] for a repository of source code. */\r\n',
+	gulp = require('gulp'),
+	lessc = require('gulp-less'),
 	gcmq = require('gulp-group-css-media-queries'),
-	insertLines = require('gulp-insert-lines');
+	insertLines = require('gulp-insert-lines'),
+	cleanCss = require('gulp-clean-css'),
+	insert = require('gulp-insert'),
+	extName = require('gulp-extname');
 
-gulp.task('default', function () {
-	gulp.src('./css/pbk-custom.css')
+gulp.task('buildMinCss', function () {
+	// First compile LESS code into CSS…
+	gulp.src('./CSS/*.less')
+		.pipe(lessc({
+			paths: ['./WSU-UE---CSS/']
+		}))
+		.pipe(gulp.dest('./CSS/'));
+	// … then group media queries in compiled CSS & minify.
+	gulp.src('./CSS/' + compiledCssSrcFileName)
 		.pipe(gcmq())
 		.pipe(insertLines({
 			'before': /^@media/,
 			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ####################################################################################################### ║\r\n*   ╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝\r\n*/',
-      'stopAfterFirstMatch': true
+			'stopAfterFirstMatch': true
 		}))
-		.pipe(gulp.dest('css'));
+		.pipe(cleanCss())
+		.pipe(insert.prepend(minCssFileHeaderStr))
+		.pipe(extName('.min.css'))
+		.pipe(gulp.dest('./CSS/'));
 });
